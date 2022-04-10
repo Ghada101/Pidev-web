8ed79bb047671deb12bb5b041cde2c8b169f640b
Ghada101 <ghada.aissa@esprit.tn>
2022-04-11 00:08:44 +0200

    hotel chain (crud)

--- src/Form/HotelchainType.php
+++ src/Form/HotelchainType.php
@@ -6,7 +6,7 @@ use App\Entity\Hotelchain;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+ 
 class HotelchainType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
