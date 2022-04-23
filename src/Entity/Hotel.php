681cd23c7c47b806b6b03edbb73fe7a966bea01f
AymenKhiari <98486365+AymenKhiari@users.noreply.github.com>
2022-04-23 18:23:20 +0100

    reservationV2

---
+++ src/Entity/Hotel.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace APP\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Hotel
+ *
+ * @ORM\Table(name="hotel", indexes={@ORM\Index(name="fk_HotelChain", columns={"fk_HotelChain"})})
+ * @ORM\Entity
+ */
+class Hotel
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_Hotel", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idHotel;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="hotel_Name", type="string", length=255, nullable=false)
+     */
+    private $hotelName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="hotel_Staff", type="string", length=255, nullable=false)
+     */
+    private $hotelStaff;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="hotel_Location", type="string", length=255, nullable=false)
+     */
+    private $hotelLocation;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="hotel_Phone", type="string", length=255, nullable=false)
+     */
+    private $hotelPhone;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="hotel_Description", type="string", length=255, nullable=false)
+     */
+    private $hotelDescription;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="hotel_Img", type="string", length=255, nullable=true)
+     */
+    private $hotelImg;
+
+    /**
+     * @var \Hotelchain
+     *
+     * @ORM\ManyToOne(targetEntity="Hotelchain")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="fk_HotelChain", referencedColumnName="id_HotelChain")
+     * })
+     */
+    private $fkHotelchain;
+
+
+}
