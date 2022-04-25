fb5f239ae55db63e2043d88bf17a9788bad0caf0
oussamasellami <oussama.sellami@esprit.tn>
2022-04-25 09:55:07 +0100

    last

---
+++ migrations/Version20220424070607.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220424070607 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event CHANGE event_time event_time VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE venue ADD latitude VARCHAR(255) DEFAULT NULL, ADD longitude VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event CHANGE event_time event_time VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE venue DROP latitude, DROP longitude');
+    }
+}
