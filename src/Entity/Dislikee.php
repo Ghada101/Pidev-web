734daee31d7ec8c74b487e2215e12ca601348147
Ghada101 <ghada.aissa@esprit.tn>
2022-05-04 02:15:25 +0200

    hotels+cars integration

---
+++ src/Entity/Dislikee.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace APP\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Dislikee
+ *
+ * @ORM\Table(name="dislikee", indexes={@ORM\Index(name="fk_commentIdd", columns={"commentId"}), @ORM\Index(name="fk_userrid", columns={"userID"})})
+ * @ORM\Entity
+ */
+class Dislikee
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="dislikeId", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $dislikeid;
+
+    /**
+     * @var \User
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="userID", referencedColumnName="id")
+     * })
+     */
+    private $userid;
+
+    /**
+     * @var \Comment
+     *
+     * @ORM\ManyToOne(targetEntity="Comment")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="commentId", referencedColumnName="comment_Id")
+     * })
+     */
+    private $commentid;
+
+
+}
